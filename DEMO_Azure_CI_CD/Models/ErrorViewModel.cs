e54cf2e26e9491f1c37cd15e9f19e6c2461f45e8
eswar <neswar2021@gmail.com>
2022-03-21 13:33:48 +0530

    Add project files.

---
+++ DEMO_Azure_CI_CD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DEMO_Azure_CI_CD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
